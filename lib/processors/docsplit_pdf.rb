1c3b4bae9e1fbfc1cb4ab17d3413b18c9873b479
sash man <sashman90@gmail.com>
2017-08-13 14:46:42 +0100

    Embedding the reraised exception's message

--- lib/processors/docsplit_pdf.rb
+++ lib/processors/docsplit_pdf.rb
@@ -12,7 +12,7 @@ module Paperclip
           Docsplit.extract_pdf(src_path, :output => dst_dir)
         end
       rescue Exception => e
-        new_exception = Paperclip::Error.new("There was an error converting #{@basename} to pdf")
+        new_exception = Paperclip::Error.new("There was an error converting #{@basename} to pdf: #{e}")
         new_exception.set_backtrace(e.backtrace)
         raise new_exception
       end
